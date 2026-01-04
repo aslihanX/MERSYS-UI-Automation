3341c271389012f48123d90fc02e096ea255e4c9
aslihanX <av.aslihangulluoglu@gmail.com>
2026-01-04 16:02:29 +0000

    US004_HamburgerMenu_ManageMsg.feature, stepDef and hamburgerMEnu page created.

---
+++ src/test/java/stepdefinitions/HamburgerMenu_SendMsgSteps.java
@@ -0,0 +1,47 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class HamburgerMenu_SendMsgSteps {
+    @Then("a text editor pop-up should be displayed")
+    public void aTextEditorPopUpShouldBeDisplayed() {
+    }
+
+    @And("the user should see and click the {string} icon")
+    public void theUserShouldSeeAndClickTheIcon(String arg0) {
+    }
+
+    @When("the user types {string} in the receiver field")
+    public void theUserTypesInTheReceiverField(String arg0) {
+    }
+
+    @Then("the system should display the results for {string}")
+    public void theSystemShouldDisplayTheResultsFor(String arg0) {
+    }
+
+    @And("the user selects the first available receiver")
+    public void theUserSelectsTheFirstAvailableReceiver() {
+    }
+
+    @And("the user enters a subject as {string}")
+    public void theUserEntersASubjectAs(String arg0) {
+    }
+
+    @And("the user types {string} in the text editor")
+    public void theUserTypesInTheTextEditor(String arg0) {
+    }
+
+    @And("the user clicks on the {string} button")
+    public void theUserClicksOnTheButton(String arg0) {
+    }
+
+    @Then("a {string} notification should be displayed")
+    public void aNotificationShouldBeDisplayed(String arg0) {
+    }
+
+    @Then("the message with subject {string} should be visible in the Outbox list")
+    public void theMessageWithSubjectShouldBeVisibleInTheOutboxList(String arg0) {
+    }
+}
