094454b844d407fa80403c1b6932975744f4ac32
tanerozc <tanerozcelik@gmail.com>
2026-01-04 16:42:18 +0000

    HamburgerMenu_FinanceAccessSteps created and updated.

---
+++ src/test/java/stepdefinitions/HamburgerMenu_FinanceAccessSteps.java
@@ -0,0 +1,8 @@
+package stepdefinitions;
+
+import org.openqa.selenium.WebDriver;
+import utils.BaseDriver;
+
+public class HamburgerMenu_FinanceAccessSteps {
+    WebDriver driver= BaseDriver.getDriver();
+}
