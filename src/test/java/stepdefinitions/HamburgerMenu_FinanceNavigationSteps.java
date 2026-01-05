e0c324db616962f6d59df3ba2fd0fb279eabf451
tanerozc <tanerozcelik@gmail.com>
2026-01-05 14:49:15 +0000

    US008_HamburgerMenu_FinanceNavigationSteps,  US008_HamburgerMenu_FinanceNavigation.feature, HomePage and FinancePage updated.

--- src/test/java/stepdefinitions/HamburgerMenu_FinanceNavigationSteps.java
+++ src/test/java/stepdefinitions/HamburgerMenu_FinanceNavigationSteps.java
@@ -1,9 +1,42 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import pages.FinancePage;
+import pages.HomePage;
 import utils.BaseDriver;
 
 public class HamburgerMenu_FinanceNavigationSteps {
 
-    WebDriver driver= BaseDriver.getDriver();
+    WebDriver driver;
+    HomePage homePage;
+    FinancePage financePage;
+
+    @When("User opens Hamburger menu")
+    public void userOpensHamburgerMenu() {
+        driver = BaseDriver.getDriver();
+        homePage = new HomePage(driver);
+        financePage = new FinancePage(driver);
+
+        homePage.openHamburgerMenu();
+        Assert.assertTrue(homePage.isHamburgerMenuOpened(), "Hamburger menu is not opened!");
+    }
+
+    @And("User clicks on Finance menu item")
+    public void userClicksOnFinanceMenuItem() {
+        homePage.clickFinanceFromHamburger();
+    }
+    @And("User clicks on My Finance menu item")
+    public void userClicksOnMyFinanceMenuItem() {
+        homePage.clickMyFinanceFromHamburger();
+    }
+
+    @Then("Finance page should be displayed")
+    public void financePageShouldBeDisplayed() {
+        financePage.verifyFinancePageOpened();
+    }
+
 }
---
+++ src/test/resources/features/US008_HamburgerMenu_FinanceNavigation.feature
@@ -0,0 +1,18 @@
+@US008
+@finance
+Feature: Hamburger Menu - Finance Navigation
+
+  As a student
+  I want to access Finance page via Hamburger menu
+  So that I can manage my finance operations
+
+  Background:
+    Given User is logged in as a student
+
+  @TC0801
+  @smoke
+  Scenario: User navigates to Finance page via Hamburger menu
+    When User opens Hamburger menu
+    And User clicks on Finance menu item
+    And User clicks on My Finance menu item
+    Then Finance page should be displayed
